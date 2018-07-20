321f900f31ae0e00e46b7c2fe6e54fa25083ab69
Adeel Ali <syedadeelalirizvi@yahoo.com>
2018-07-21 03:18:15 +0500

    Added two files for prod and master. however active and in use file is app.module.ts
    Before creating prod build we have to remove mockcamera function and uncomment camera and comment mock camera
    
    you can see difference in these two files.

---
+++ src/app/app.module-prod.ts
@@ -0,0 +1,110 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { ErrorHandler, NgModule } from '@angular/core';
+import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
+import { SplashScreen } from '@ionic-native/splash-screen';
+import { StatusBar } from '@ionic-native/status-bar';
+import { Camera } from '@ionic-native/camera';
+import { HttpClientModule } from '@angular/common/http';
+import { IonicStorageModule } from '@ionic/storage';
+import { MyApp } from './app.component';
+import { SignaturePage } from '../pages/signature/signature';
+import { ChangepasswordPage } from '../pages/changepassword/changepassword';
+import { HomePage } from '../pages/home/home';
+import { FailDuePage } from '../pages/fail-due/fail-due';
+import { ForgotPasswordPage } from '../pages/forgot-password/forgot-password';
+import { InformationPage } from '../pages/information/information';
+import { InspectionRemarksPage } from '../pages/inspection-remarks/inspection-remarks';
+import { LiftingPage } from '../pages/lifting/lifting';
+import { MainPage } from '../pages/main/main';
+import { ModalPage } from '../pages/modal/modal';
+
+import { OwnCatPage } from '../pages/own-cat/own-cat';
+import { OwnSubCatPage } from '../pages/own-sub-cat/own-sub-cat';
+import { PassObservationPage } from '../pages/pass-observation/pass-observation';
+import { PassSafePage } from '../pages/pass-safe/pass-safe';
+import { PreviousPage } from '../pages/previous/previous';
+import { ProfilePage } from '../pages/profile/profile';
+import { RemarksPage } from '../pages/remarks/remarks';
+import { SafetyPage } from '../pages/safety/safety';
+import { SafetyCatInfoPage } from '../pages/safety-cat-info/safety-cat-info';
+import { SetpasswordPage } from '../pages/setpassword/setpassword';
+import { SignupPage } from '../pages/signup/signup';
+import { VerificationPage } from '../pages/verification/verification';
+import { WorkPage } from '../pages/work/work';
+import { SignaturePadModule } from 'angular2-signaturepad';
+
+@NgModule({
+  declarations: [
+    MyApp,
+	HomePage,
+    ChangepasswordPage,
+    HomePage,
+    MainPage,
+    ModalPage,
+    FailDuePage,
+    ForgotPasswordPage,
+    InformationPage,
+    InspectionRemarksPage,
+    LiftingPage,
+    OwnCatPage,
+    OwnSubCatPage,
+    PassObservationPage,
+    PassSafePage,
+    PreviousPage,
+    ProfilePage,
+    RemarksPage,
+    SafetyPage,
+    SafetyCatInfoPage,
+    SetpasswordPage,
+    SignupPage,
+	SignaturePage,
+    VerificationPage,
+    WorkPage
+	
+  ],
+  imports: [
+    BrowserModule,
+	HttpClientModule,
+	SignaturePadModule,
+	IonicStorageModule.forRoot({
+      name: '__mydb',
+         driverOrder: ['indexeddb', 'sqlite', 'websql']
+    }),
+    IonicModule.forRoot(MyApp)
+  ],
+  bootstrap: [IonicApp],
+  entryComponents: [
+    MyApp,
+    ChangepasswordPage,
+	SignaturePage,
+    HomePage,
+    MainPage,
+    ModalPage,
+    FailDuePage,
+    ForgotPasswordPage,
+    InformationPage,
+    InspectionRemarksPage,
+    LiftingPage,
+    OwnCatPage,
+    OwnSubCatPage,
+    PassObservationPage,
+    PassSafePage,
+    PreviousPage,
+    ProfilePage,
+    RemarksPage,
+    SafetyPage,
+    SafetyCatInfoPage,
+    SetpasswordPage,
+    SignupPage,
+    VerificationPage,
+    WorkPage
+  ],
+  providers: [
+    StatusBar,
+    SplashScreen,
+	Camera,
+	//{ provide: Camera, useClass: CameraMock },
+    {provide: ErrorHandler, useClass: IonicErrorHandler}
+  ]
+})
+export class AppModule {}
